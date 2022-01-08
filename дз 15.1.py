96c680473922d807b90f87798bad02f059047b80
tvoigoroshek <92248290+tvoigoroshek@users.noreply.github.com>
2022-01-09 00:52:16 +0300

    Add files via upload

---
+++ дз 15.1.py
@@ -0,0 +1,16 @@
+import random
+i=print(random.randint(0, 100))
+int==type(i)
+def meow():
+    i=int(input("""Пожалуйста,введите это число сюда еще раз.
+Компьютер не распознает его как Integer((((:"""))
+    if i > 50:
+        for i in range (i):
+            print(i)
+    if i < 50:
+        for i in range(i):
+            print([0,1])
+    if i==50:
+        print("Вам повезло!!!")
+    
+meow()
---
+++ дз 15.2.py
@@ -0,0 +1,14 @@
+import random
+a=random.randint(0, 10)
+b=random.randint(0, 10)
+c=[5,8,1,6,0]
+p=random.randint(-20, 20)
+if a > b:
+     c.append(a)
+     print(c)
+if a < b:
+     for p in range(10):
+         print(p)
+     
+    
+    
